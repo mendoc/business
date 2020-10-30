a127d4b31d588d756f7dce0ef7ebaabbc82ea8e8
nickqadosh <nickqadosh@gmail.com>
2020-10-30 17:00:09 +0100

    ajout de fonction: fonction pour récupérer un candidat

--- application/models/Candidat_model.php
+++ application/models/Candidat_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Candidat extends CI_Model
+class Candidat_model extends CI_Model
 {
 
     public $nom_prenom;
@@ -30,7 +30,7 @@ class Candidat extends CI_Model
 
     public function ajouter_candidat($params) //fonction pour ajouter un candidat
     {
-        $this->nom_prenom = $params['nom_prenom']; 
+        $this->nom_prenom = $params['nom_prenom'];
         $this->num_tel    = $params['num_tel'];
         $this->num_what   = $params['num_what'];
         $this->email      = $params['email'];
@@ -44,4 +44,33 @@ class Candidat extends CI_Model
 
         return $this->db->insert('eb_candidat', $this);
     }
+
+    public function supprimer_candidat($id) //fonction pour supprimer un candidat
+    {
+     
+        return $this->db->delete('eb_candidat', array('id_can' => $id));
+    }
+
+    public function modifier_candidat($params) //fonction pour modifier un candidat
+    {
+        $this->nom_prenom = $params['nom_prenom'];
+        $this->num_tel    = $params['num_tel'];
+        $this->num_what   = $params['num_what'];
+        $this->email      = $params['email'];
+        $this->sexe       = $params['sexe'];
+        $this->date_n     = $params['date_n'];
+        $this->domaine_act   = $params['domaine_act'];
+        $this->type_serv = $params['type_serv'];
+        $this->attentes = $params['attentes'];
+        $this->horaire = $params['horaire'];
+        $this->id_res_part = $params['id_res_part'];
+
+        return $this->db->update('eb_candidat', $this, array('id' => $params['id']));
+    }
+
+    public function selectionner_candidat($id) // fonction pour récupérer un candidat spécifique
+    {
+        return $this->db->get_where('eb_candidat', array('id_can'=>$id));  
+    }
+
 }
