86dbb4804899ba510f14e7d40b1fe2f8085557d3
yanick <you@example.com>
2020-10-30 13:12:47 +0100

    Ressources pdf

---
+++ application/views/back/commercial/pdf_fichier.php
@@ -0,0 +1,109 @@
+<div class="my-3 my-md-5">
+          <div class="container">
+            <div class="page-header">
+              <h1 class="page-title">
+                PDF
+              </h1>
+            </div>
+            <div class="row row-cards">
+              <div class="col-lg-3">
+                <div class="row">
+                  <div class="col-md-6 col-lg-12">
+                    <div class="card">
+                      <div class="card-body">
+                        <div class="mb-4 text-center">
+                          <img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="Apple iPhone 7 128GB" class="img-fluid">
+                        </div>
+                        <h4 class="card-title"><a href="javascript:void(0)">Comment lancer son business en 15 jours</a></h4>
+                        <div class="card-subtitle">
+                          Ecole241 Business
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                 
+                </div>
+              </div>
+
+              <div class="col-lg-9">
+                <div class="card">
+                  <table class="table card-table table-vcenter">
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Transformation digitale de votre entreprise
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Comment avoir plus de client
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                    </tr>
+
+
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Stratégies marketing digitale
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Comment se positonner sur le marché
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Les outils dont vous avez besoin
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                      
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Les outils de métriques
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                     
+
+                    </tr>
+                    <tr>
+                      <td><img src="d<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        AARRR
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+                    
+                    </tr>
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Le growth haking
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td>
+
+                    </tr>
+                    <tr>
+                      <td><img src="<?= theme_url() ?>assets/images/pdf_img/view_pdf.png" alt="" class="h-8"></td>
+                      <td>
+                        Comment lancer son business en 15 jours
+                        <div class="badge badge-default badge-md"><i class="fe fe-share-2"></i></div>
+                      </td> 
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
