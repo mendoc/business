dabab7a804ad7fc50f5e525c1c01a2cac80e6692
OKARLES <louecharles35@gmail.com>
2020-10-30 18:05:43 +0100

    Interface de la liste des ressources du coté du commercial

---
+++ application/views/back/commercial/ressources.php
@@ -0,0 +1,179 @@
+            <div class="container h-auto">
+              <div class="page-header">
+                <h1 class="page-title">
+                  Images
+                </h1>
+                <div class="page-subtitle">1 - 12 of 1713 photos</div>
+                <div class="page-options d-flex">
+                  <select class="form-control custom-select w-auto">
+                    <option value="asc">Newest</option>
+                    <option value="desc">Oldest</option>
+                  </select>
+                  <div class="input-icon ml-2">
+                    <span class="input-icon-addon">
+                      <i class="fe fe-search"></i>
+                    </span>
+                    <input type="text" class="form-control w-10" placeholder="Search photo">
+                  </div>
+                </div>
+              </div>
+              <div class="row row-cards">
+                <div class="col-sm-6 col-md-3">
+                  <div class="card p-3">
+                    <a href="javascript:void(0)" class="mb-3">
+                      <img src="<?= theme_url() ?>demo/photos/grant-ritchie-338179-500.jpg" alt="Photo by Nathan Guerrero" class="rounded">
+                    </a>
+                    <div class="d-flex align-items-center px-2">
+                      <div>
+                        <div>Nathan Guerrero</div>
+                        <small class="d-block text-muted">Il y a 5 minutes</small>
+                      </div>
+                      <div class="ml-auto text-muted">
+                        <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4 col-md-3">
+                  <div class="card p-3">
+                    <a href="javascript:void(0)" class="mb-3">
+                      <img src="<?= theme_url() ?>demo/photos/ilnur-kalimullin-218996-500.jpg" alt="Photo by Alice Mason" class="rounded">
+                    </a>
+                    <div class="d-flex align-items-center px-2">
+                      <div>
+                        <div>Alice Mason</div>
+                        <small class="d-block text-muted">Il y a 4 jours</small>
+                      </div>
+                      <div class="ml-auto text-muted">
+                        <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4 col-md-3">
+                  <div class="card p-3">
+                    <a href="javascript:void(0)" class="mb-3">
+                      <img src="<?= theme_url() ?>demo/photos/jakob-owens-224352-500.jpg" alt="Photo by Rose Bradley" class="rounded">
+                    </a>
+                    <div class="d-flex align-items-center px-2">
+                      <div>
+                        <div>Rose Bradley</div>
+                        <small class="d-block text-muted">Il y a une semaine</small>
+                      </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="page-header">
+        <h1 class="page-title">
+          Videos
+        </h1>
+        <div class="page-subtitle">1 - 12 of 150 videos</div>
+        <div class="page-options d-flex">
+          <select class="form-control custom-select w-auto">
+            <option value="asc">Newest</option>
+            <option value="desc">Oldest</option>
+          </select>
+          <div class="input-icon ml-2">
+            <span class="input-icon-addon">
+              <i class="fe fe-search"></i>
+            </span>
+            <input type="text" class="form-control w-10" placeholder="Search video">
+          </div>
+        </div>
+      </div>
+      <div class="row row-cards">
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-3">
+            <i class="fe fe-feplay"></i>
+            <a href="javascript:void(0)" class="mb-3">
+              <div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/ZcPJ3gkLzto" frameborder="0" allowfullscreen></iframe></div>
+            </a>
+            <div class="d-flex align-items-center px-2">
+              <div>
+                <div>Nathan Guerrero</div>
+                <small class="d-block text-muted">il y'a 2 jours</small>
+              </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-3">
+            <a href="javascript:void(0)" class="mb-3">
+              <div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/ZcPJ3gkLzto" frameborder="0" allowfullscreen></iframe></div>
+            </a>
+            <div class="d-flex align-items-center px-2">
+              <div>
+                <div>Alice Mason</div>
+                <small class="d-block text-muted">il y a une semaine</small>
+              </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-3">
+            <a href="javascript:void(0)" class="mb-3">
+              <div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/ZcPJ3gkLzto" frameborder="0" allowfullscreen></iframe></div>
+            </a>
+            <div class="d-flex align-items-center px-2">
+              <div>
+                <div>Rose Bradley</div>
+                <small class="d-block text-muted">il y'a 4 jours</small>
+              </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-3">
+            <a href="javascript:void(0)" class="mb-3">
+              <div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/ZcPJ3gkLzto" frameborder="0" allowfullscreen></iframe></div>
+            </a>
+            <div class="d-flex align-items-center px-2">
+              <div>
+                <div>Peter Richards</div>
+                <small class="d-block text-muted">Il y a 16 jours</small>
+              </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="card p-3">
+            <a href="javascript:void(0)" class="mb-3">
+              <div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/ZcPJ3gkLzto" frameborder="0" allowfullscreen></iframe></div>
+            </a>
+            <div class="d-flex align-items-center px-2">
+              <div>
+                <div>Wayne Holland</div>
+                <small class="d-block text-muted">Il y a 18 jours</small>
+              </div>
+                    <div class="ml-auto text-muted">
+                      <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-share-2 mr-1"></i></a>
+                    </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
