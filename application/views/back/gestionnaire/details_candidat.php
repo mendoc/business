b7d855faf1506a7087cf5a72324f90f8790ba64b
yanick <you@example.com>
2020-10-31 14:11:31 +0100

    ajout de vue détails du candidat

---
+++ application/views/back/gestionnaire/details_candidat.php
@@ -0,0 +1,120 @@
+<div class="my-3 my-md-5">
+        <div class="details_centre">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Détails du candidat</h3>
+            </div>
+            <div class="card-body">
+              <div class="block_details">
+                <h5>Nom:</h5>
+                <p>Moussounda</p>
+              </div>
+
+              <div class="block_details">
+                <h5>prénom:</h5>
+                <p>Yanick</p>
+              </div>
+
+              <div class="block_details">
+                <h5>sexe:</h5>
+                <p>H</p>
+              </div>
+
+              <div class="block_details">
+                <h5>date de naissance:</h5>
+                <p>22/09/1999</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Adresse e-amil:</h5>
+                <p>Moussounda</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Numéro de téléphone:</h5>
+                <p>062 20 60 20</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Numéro de téléphone whatsapp:</h5>
+                <p>062 20 60 20</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Horaire de formation:</h5>
+                <p>Matinée</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Domaine d'activité:</h5>
+                <p>Artisana</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Type de service:</h5>
+                <p>Artisana</p>
+              </div>
+
+              <div class="block_details">
+                <h5>Attentes:</h5>
+                <p>Artisana</p>
+              </div>
+            </div>
+          </div> 
+          <div class="col-lg-6">
+            
+            <form class="mb-4">
+              <div class="form-row">
+                <div class="col">
+                  <input type="number" min="5000" class="form-control" placeholder="Montant">
+                </div>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Motif">
+                </div>
+                <button type="submit" class="btn btn-primary">valider</button>
+              </div>
+            </form>
+
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Liste de demandes de retraits</h3>
+            </div>
+
+            <div class="table-responsive">
+              <table class="table card-table table-striped table-vcenter">
+                <tbody>
+                  <tr>
+                    <td>Ronald Bradley</td>
+                    <td>Initial commit</td>
+                    <td class="text-nowrap">May 6, 2018</td>
+
+                  </tr>
+                  <tr>
+                    <td>Russell Gibson</td>
+                    <td>Main structure</td>
+                    <td class="text-nowrap">April 22, 2018</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Beverly Armstrong</td>
+                    <td>Left sidebar adjustments</td>
+                    <td class="text-nowrap">April 15, 2018</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Bobby Knight</td>
+                    <td>Topbar dropdown style</td>
+                    <td class="text-nowrap">April 8, 2018</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Sharon Wells</td>
+                    <td>Fixes #625</td>
+                    <td class="text-nowrap">April 9, 2018</td>
+                    <td> </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+      </div>
