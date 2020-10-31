38ec02c66dd3a63a241ccd6858e625d6448b011f
edwigelily <bamenouedwige@gmail.com>
2020-10-31 10:49:03 +0100

    Le crud du model Retrait

---
+++ application/models/Retrait_model.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Retrait_model extends CI_Model
+{
+
+    public $montant_retrait;
+    public $date_fin;
+    public $date_debut;
+    public $id_com;
+    public $id_gest;
+
+
+    // Nom de la table
+    private $table = 'retrait';
+    
+    // Clé primaire de la table
+    private $id = 'id_ret';
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function tout()
+    {
+        $query = $this->db->get($this->table);
+        return $query->result();
+    }
+
+    public function ajouter()
+    {
+        return $this->db->insert($this->table, $this);
+    }
+
+    public function modifier($id)
+    {
+        return $this->db->update($this->table, $this, array($this->id => $id));
+    }
+
+    public function supprimer($id)
+    {
+        return $this->db->delete($this->table, array($this->id => $id));
+    }
+}
