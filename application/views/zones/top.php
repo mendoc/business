96df1d33167c442c8031b3b1e6f278bce2f158d8
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-10-31 18:24:08 +0100

    Confirmer le paiment

--- application/views/zones/top.php
+++ application/views/zones/top.php
@@ -30,7 +30,7 @@
             <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-lg-3 ml-auto">
+                        <div class="col-lg-3 ml-auto d-none">
                             <form class="input-icon my-3 my-lg-0">
                                 <input type="search" class="form-control header-search" placeholder="Rechercher" tabindex="1">
                                 <div class="input-icon-addon">
