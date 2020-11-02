56170dc7d2ae520ea4ce7cedaab00fa5c6ab03b6
yanick <you@example.com>
2020-11-02 14:25:42 +0100

    ajout du trafic des ressources chez le gestinnaire

---
+++ application/views/back/gestionnaire/trafic_ressources.php
@@ -0,0 +1,91 @@
+<div class="my-3 my-md-5">
+
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Consultation le trafic d'une ressource</h3>
+        </div>
+        <div class="table-responsive">
+          <table class="table card-table table-vcenter text-nowrap">
+            <thead>
+              <tr>
+                <th>Le nombre de clics</th>
+                <th>Le nombre de partages</th>
+                <th>Le nombre d'inscrits</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                 100
+                </td>
+                <td>
+                  1002
+                </td>
+                <td>
+                  100
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  83
+                </td>
+                <td>
+                  1000
+                </td>
+                <td>
+                  75
+                </td>
+
+              </tr>
+              <tr>
+                <td>
+                  75
+                </td>
+                <td>
+                  61
+                </td>
+                <td>
+                  70
+                </td>
+
+              </tr>
+              <tr>
+                <td>
+                 73
+                </td>
+                <td>
+                  55
+                </td>
+                <td>
+                 40
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  60
+                </td>
+                <td>
+                  50
+                </td>
+                <td>
+                  38
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  50
+                </td>
+                <td>
+                 43
+                </td>
+                <td>
+                  20
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
