028054d3fbbf52ff0eab6639acebae3ca26124ce
edwigelily <bamenouedwige@gmail.com>
2020-11-02 10:46:27 +0100

    dump modifié

--- dump/ecole241business.sql
+++ dump/ecole241business.sql
@@ -35,7 +35,7 @@ CREATE TABLE `eb_candidat` (
   `attentes` text,
   `horaire` varchar(10) NOT NULL,
   `id_res_part` int(11) DEFAULT NULL,
-  `type_cours` char(1) NOT NULL,
+  `type_cours` char(1) NOT NULL DEFAULT 'P',
   PRIMARY KEY (`id_can`),
   UNIQUE KEY `email` (`email`),
   KEY `id_res_part` (`id_res_part`),
@@ -154,7 +154,7 @@ CREATE TABLE `eb_ressource` (
   `lien` text,
   `fichier` varchar(255) DEFAULT NULL,
   `date_res` datetime DEFAULT CURRENT_TIMESTAMP,
-  `type_res` varchar(255) NOT NULL,
+  `type_res` varchar(15) DEFAULT NULL,
   `id_them` int(11) DEFAULT NULL,
   `id_gest` int(11) NOT NULL,
   PRIMARY KEY (`id_res`),
@@ -270,4 +270,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-02  9:35:56
+-- Dump completed on 2020-11-02 10:44:57
