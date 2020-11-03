72229e969f04eb661a1ccc49c52cdf845c81368e
dasilva218 <57779162+dasilva218@users.noreply.github.com>
2020-11-03 18:01:15 +0100

    validation des champs première amélioration

--- application/controllers/Candidat.php
+++ application/controllers/Candidat.php
@@ -6,57 +6,98 @@ class Candidat extends CI_Controller
     //La fonction qui affiche la vue candidat
     public function index()
     {
+        // On valide les informations
+        $this->form_validation->set_rules('nom', 'Nom', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('prenom', 'Prenom', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('sexe', 'Sexe', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('date', 'Date', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('email', 'email', 'required|valid_emails|regex_match[#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#]', array(
+            'required' => 'Le champ %s est obligatoire',
+            'valid_emails' => 'Le champ %s n\'est pas valide',
+            'regex_match' => 'Le champ %s n\'est pas valide'
+        ));
+        $this->form_validation->set_rules('telephone', 'telephone', 'required|regex_match[/^0(62|74|77|65|66|11)\d{6}$/]', array(
+            'required' => 'Le champ %s est obligatoire',
+            'regex_match' => 'Le format du %s n\'est pas valide'
+        ));
+        $this->form_validation->set_rules('numero-whatsapp', 'numero-whatsapp', 'required|regex_match[/^0(62|74|77|65|66)\d{6}$/]', array(
+            'required' => 'Le champ %s est obligatoire',
+            'regex_match' => 'Le format du %s n\'est pas valide'
+        ));
+        $this->form_validation->set_rules('horaire', 'horaire', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('domaine', 'horaire', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('service', 'service', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        $this->form_validation->set_rules('attentes', 'attentes', 'required', array(
+            'required' => 'Le champ %s est obligatoire'
+        ));
+        // si condition ok !
+        if ($this->form_validation->run()) {
+            // On récupère les infos du formulaire
+            $nom         = $this->input->post('nom');
+            $prenom      = $this->input->post('prenom');
+            $sexe        = $this->input->post('sexe');
+            $date_n      = $this->input->post('date');
+            $email       = $this->input->post('email');
+            $num_tel     = $this->input->post('telephone');
+            $num_what    = $this->input->post('numero-whatsapp');
+            $horaire     = $this->input->post('horaire');
+            $domaine_act = $this->input->post('domaine');
+            $type_serv   = $this->input->post('service');
+            $attentes    = $this->input->post('attentes');
+            // On crée un candidat
+            $candidat = new Candidat_model();
+
+            $candidat->nom_prenom  = $nom . ' ' . $prenom;
+            $candidat->num_tel     = $num_tel;
+            $candidat->num_what    = $num_what;
+            $candidat->email       = $email;
+            $candidat->sexe        = $sexe;
+            $candidat->date_n      = $date_n;
+            $candidat->domaine_act = $domaine_act;
+            $candidat->type_serv   = $type_serv;
+            $candidat->attentes    = $attentes;
+            $candidat->horaire     = $horaire;
+
+            // On enregistre le candidat dans la base de données
+            $succes = $candidat->s_enregistrer();
+
+            // On le redirige en fonction du résultat de la requete
+            if ($succes) {
+                $message = "Bonjour " . ($sexe == 'F' ? 'Mme.' : 'M.') . " " . $candidat->nom_prenom . ", \n\nNous avons bien reçu votre inscription au programme de formation Ecole 241 Business destiné aux commerçants et artisants.
+            \n\nCi-dessous, les informations que vous avez fournies lors de votre enregistrement.\n\nMerci pour la confiance accordée, nous vous disons à bientôt.\n\n L'équipe Ecole 241 Business.";
+
+                // On envoie d'un mail au candidat
+                mail($email, 'Ecole 241 Business - Inscription', $message);
+
+                redirect('candidat/inscription_reussi');
+                die();
+            } else {
+                redirect('candidat');
+            }
+        }
+
         $this->load->view('front/candidat/inscription');
     }
 
-    // la fonction qui réupere les donnees du formulaire
+    /* la fonction qui réupere les données du formulaire
     public function traitement_enregistrement()
     {
-        // On récupère les infos du formulaire
-        $nom         = $this->input->post('nom');
-        $prenom      = $this->input->post('prenom');
-        $sexe        = $this->input->post('sexe');
-        $date_n      = $this->input->post('date');
-        $email       = $this->input->post('email');
-        $num_tel     = $this->input->post('telephone');
-        $num_what    = $this->input->post('numero-whatsapp');
-        $horaire     = $this->input->post('horaire');
-        $domaine_act = $this->input->post('domaine');
-        $type_serv   = $this->input->post('service');
-        $attentes    = $this->input->post('attentes');
-
-        // On valide les informations
-
-        // On crée un candidat
-        $candidat = new Candidat_model();
-
-        $candidat->nom_prenom  = $nom . ' ' . $prenom;
-        $candidat->num_tel     = $num_tel;
-        $candidat->num_what    = $num_what;
-        $candidat->email       = $email;
-        $candidat->sexe        = $sexe;
-        $candidat->date_n      = $date_n;
-        $candidat->domaine_act = $domaine_act;
-        $candidat->type_serv   = $type_serv;
-        $candidat->attentes    = $attentes;
-        $candidat->horaire     = $horaire;
-
-        // On enregistre le candidat dans la base de données
-        $succes = $candidat->s_enregistrer();
-
-        // On le redirige en fonction du résultat de la requete
-        if ($succes) {
-            $message = "Bonjour " . ($sexe == 'F' ? 'Mme.' : 'M.') . " " . $candidat->nom_prenom . ", \n\nNous avons bien reçu votre inscription au programme de formation Ecole 241 Business destiné aux commerçants et artisants.
-            \n\nCi-dessous, les informations que vous avez fournies lors de votre enregistrement.\n\nMerci pour la confiance accordée, nous vous disons à bientôt.\n\n L'équipe Ecole 241 Business.";
-
-            // On envoie un mail au candidat
-            mail($email, 'Ecole 241 Business - Inscription', $message);
-
-            redirect('candidat/inscription_reussi');
-        } else {
-            redirect('candidat');
-        }
-    }
+        
+    }*/
 
     public function inscription_reussi()
     {
--- application/views/front/candidat/inscription.php
+++ application/views/front/candidat/inscription.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Enregistrement candidat - Ecole 241 Business</title>
-    
+
     <link rel="stylesheet" type="text/css" href="<?= theme_url() ?>assets/css/formulaire_candidat.css">
 </head>
 
@@ -22,36 +22,42 @@
                 <h2 class="animation a1">Formulaire du candidat</h2>
             </div>
             <!--===*** Start of Form ***===-->
-            <form class="form" action="<?= site_url('candidat/traitement_enregistrement') ?>" method="POST">
-                <input type="nom" name="nom" class="form-field animation a3" placeholder="Nom Ex. SAMBA" required>
-                <input type="prenom" name="prenom" class="form-field animation a3" placeholder="Prenom Ex. Pascal" required>
 
+            <form class="form" action="<?= site_url('candidat/index') ?>" method="POST">
+                <input type="nom" name="nom" class="form-field animation a3" placeholder="Nom Ex. SAMBA" value="<?= set_value('nom'); ?>">
+                <?= form_error('nom') ?>
+                <input type="prenom" name="prenom" class="form-field animation a3" placeholder="Prenom Ex. Pascal" value="<?= set_value('prenom'); ?>">
+                <?= form_error('prenom') ?>
                 <fieldset class="animation a3">
                     <label for="name">Votre sexe</label>
-                    <input type="radio" name="sexe" value="H" checked required><label for="homme">H</label>
-                    <input type="radio" name="sexe" value="F" required><label for="femme">F</label>
+                    <input type="radio" name="sexe" value="H" checked><label for="homme">H</label>
+                    <input type="radio" name="sexe" value="F"><label for="femme">F</label>
                 </fieldset>
 
                 <fieldset class="animation a3">
                     <label for="name">Date de naissance</label>
-                    <input type="date" value="1989-12-01" name="date" class="form-field animation a3" required>
+                    <input type="date" value="1989-12-01" name="date" class="form-field animation a3">
                 </fieldset>
 
-                <input type="email" name="email" class="form-field animation a3" placeholder="Adresse mail" required>
-                <input type="tel" name="telephone" class="form-field animation a3" placeholder="Numéro de téléphone Ex. 077 01 89 00" required>
-                <input type="tel" name="num_what" class="form-field animation a3" placeholder="Numéro de téléphone WhatsApp Ex. 077 01 89 00">
-
+                <input type="email" name="email" class="form-field animation a3" placeholder="Adresse mail" value="<?= set_value('email'); ?>">
+                <?= form_error('email') ?>
+                <input type="tel" name="telephone" class="form-field animation a3" placeholder="Numéro de téléphone Ex. 077 01 89 00" value="<?= set_value('telephone'); ?>">
+                <?= form_error('telephone') ?>
+                <input type="tel" name="numero-whatsapp" value="<?= set_value('num_what'); ?>" class="form-field animation a3" placeholder="Numéro de téléphone WhatsApp Ex. 077 01 89 00">
+                <?= form_error('numero-whatsapp') ?>
                 <fieldset class="animation a3">
                     <label for="horaire">Horaire</label>
-                    <input type="radio" name="horaire" checked value="Matin" required><label for="horaire">Matin</label>
-                    <input type="radio" name="horaire" value="Après-midi" required><label for="horaire">Après-midi</label>
+                    <input type="radio" name="horaire" checked value="Matin"><label for="horaire">Matin</label>
+                    <input type="radio" name="horaire" value="Après-midi"><label for="horaire">Après-midi</label>
                 </fieldset>
 
-                <input type="text" name="domaine" class="form-field animation a3" placeholder="Votre domaine d’activité Ex. Artisanat">
-                <input type="text" name="service" class="form-field animation a3" placeholder="Type de service">
-
+                <input type="text" name="domaine" value="<?= set_value('domaine'); ?>" class="form-field animation a3" placeholder="Votre domaine d’activité Ex. Artisanat">
+                <?= form_error('domaine') ?>
+                <input type="text" name="service" value="<?= set_value('service'); ?>" class="form-field animation a3" placeholder="Type de service">
+                <?= form_error('service') ?>
                 <div data-validate="Message is requiredd" class="animation a3">
-                    <textarea type="text" class="input100" name="attentes" placeholder="Vos attentes par rapport à cette formation"></textarea>
+                    <textarea type="text" class="input100" name="attentes" value="<?= set_value('attentes') ?>" placeholder="Vos attentes par rapport à cette formation"></textarea>
+                    <?= form_error('attentes') ?>
                 </div>
                 <button class="animation a6">S’enregistrer</button>
             </form>
