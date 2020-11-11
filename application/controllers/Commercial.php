6114a85ab728530de4c526f067fad77a3f048996
dasilva218 <57779162+dasilva218@users.noreply.github.com>
2020-11-11 13:20:28 +0100

    Ajout de l'entête header

--- application/controllers/Commercial.php
+++ application/controllers/Commercial.php
@@ -138,12 +138,10 @@ class Commercial extends CI_Controller
             $this->session->set_userdata('hash', $commercial->hash);
             $this->session->set_userdata('raccourci', $commercial->raccourci);
             // On envoie d'un mail au candidat
-            $message = "Bonjour " . ($sexe == 'F' ? 'Mme.' : 'M.') . " " . $inscrit->nom_prenom . ", \n\nNous avons bien reçu votre inscription comme commercial à L'école 241 Business."
-            ;
-
+            $message = "Bonjour " . ($sexe == 'F' ? 'Mme.' : 'M.') . " " . $inscrit->nom_prenom . ", \n\nNous avons bien reçu votre inscription comme commercial à L'école 241 Business.";
+            $headers = "From: Ecole 241 Business <contact@business.ecole241.org>\r\n";
             // On envoie d'un mail au candidat
-            mail($email, 'Ecole 241 Business - Inscription', $message);
-            
+            mail($email, 'Ecole 241 Business - Inscription', $message, $headers);
             redirect('commercial');
         } else {
             redirect('commercial/inscription');
