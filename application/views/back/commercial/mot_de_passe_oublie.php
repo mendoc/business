e8d0a1f463fcfe4bf2e34d5217dc99c084efd300
edwigelily <bamenouedwige@gmail.com>
2020-11-13 10:49:13 +0100

    titre mot de passe oublié

--- application/views/back/commercial/mot_de_passe_oublie.php
+++ application/views/back/commercial/mot_de_passe_oublie.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= theme_url() ?>/assets/css/connexion_commercial.css">
-    <title>Formulaire</title>
+    <title>Mot de passe oublié commercial</title>
 </head>
 
 <body>
