b81bc30a2d0f60f52ff4c1a7bdd4452f04daf0a8
dynamo63 <smoulongui@gmail.com>
2020-11-17 14:13:15 +0100

    Changement de l'email inscription gestionnaire

--- application/views/email/gestionnaire/inscription.php
+++ application/views/email/gestionnaire/inscription.php
@@ -55,14 +55,14 @@ font-family: 'Montserrat', sans-serif;
             margin-bottom: 0.60em" class=" title ">Votre compte a été créé</h1>
             <!--# le message 1 #--->
             <p style="margin-bottom: 10px;
-            padding-top: 2em;font-size: 1rem;">Bonjour Abiba {NOM},</p>
+            padding-top: 2em;font-size: 1rem;">Bonjour {NOM},</p>
             <p style=" margin-top: 30px;
             margin-bottom: 20px;font-size: 1rem;">Votre compte gestionnaire a bien été créé.</p>
             <!--# le message 2 #--->
             <p style="margin-top: 20px;
             margin-bottom: 20px;font-size: 1rem;">Ci-dessous, les informations pour vous connecter.</p>
             <p style=" margin-top: 30px;
-            margin-bottom: 20px;font-size: 1rem; word-wrap: break-word;">Lien de connexion : {LIEN}</p>
+            margin-bottom: 20px;font-size: 1rem; word-wrap: break-word;">Lien de connexion : <?= site_url('gestionnaire/connexion') ?></p>
             <p style="margin-top: 20px;
             margin-bottom: 20px;font-size: 1rem;">Adresse e-mail : {EMAIL}</p>
             <!--# le message 3 #--->
@@ -83,7 +83,7 @@ font-family: 'Montserrat', sans-serif;
             margin-top: 2.50em;
             margin-bottom: 2.50em;
             border-radius: 5px;text-decoration: none;
-        ;" href="# ">Accéder au tableau de bord</a>
+        ;" href="<?= site_url('gestionnaire/connexion') ?>">Accéder au tableau de bord</a>
             <!--# le message 4 #--->
             <p class="name-team " style="font-size: 1rem;font-family: 'Montserrat', sans-serif; font-style: italic; margin-bottom: 4em; ">L'équipe Ecole 241 Business.</p>
         </div>
