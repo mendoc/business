916253c801065f03c80e373c5e4989576bbd3286
dynamo63 <smoulongui@gmail.com>
2020-11-23 11:11:33 +0100

    listing candidat: mis en orange des prix

--- application/views/back/gestionnaire/candidats.php
+++ application/views/back/gestionnaire/candidats.php
@@ -33,7 +33,7 @@
                                     <td><a href="<?= site_url('gestionnaire/detail_candidat/' . $candidat->id_can) ?>"><?= $candidat->nom_prenom; ?></a></td>
                                     <td><?= isset($candidat->nom_com) ? $candidat->nom_com : 'Aucun' ?></a></td>
                                     <td><?= $candidat->type_cours == 'P' ? 'En presentiel' : 'En ligne' ?></td>
-                                    <td class="<?= $candidat->max_montant == $candidat->montant ? 'text-success font-weight-bold' : '' ?>">
+                                    <td class="<?= $candidat->max_montant == $candidat->montant ? 'text-success font-weight-bold' : ($candidat->montant > 0 ? 'font-weight-bold text-warning' : '') ?>">
                                         <?=  number_format($candidat->montant, 0, ',', ' ');  ?> F CFA
                                     </td>
                                     <td class="<?= ($candidat->max_montant - $candidat->montant) > 0 ? 'text-danger font-weight-bold' : '' ?>">
